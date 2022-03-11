01f8f0e3707ab340661970bdfd0196cc40d1549d
P1 <jack@garzafamily.org>
2022-03-11 09:43:49 -0800

    Changes

--- webapp.py
+++ webapp.py
@@ -10,6 +10,29 @@ def render_main():
 
 @app.route("/response")
 def render_response():
+    #Store possible races in the race dictionary
+    races = {
+        "fifteenHundred": 1500,
+        "1Mile": 1609,
+        "threeK": 3000,
+        "3mile": 4828,
+        "fiveK": 5000,
+        "tenK": 10000,
+        "halfMarathon": 21097,
+        "Marathon": 42195
+    }
+    #Store readable string equivalent names for the races
+    raceResponse = {
+        "fifteenHundred": "1500M",
+        "1Mile": "1 Mile",
+        "threeK": "3K",
+        "3mile": "3 Mile",
+        "fiveK": "5K",
+        "tenK": "10K",
+        "halfMarathon": "Half Marathon",
+        "Marathon": "Marathon"
+    }
+
     #If the user does not include a time for the optional field, do not use it to compute
     if(request.args['pastRaceHr2'] == "" and request.args['pastRaceMin2'] == "" and request.args['pastRaceSec2'] == ""):
         #variables assigned to information used for calculations
@@ -21,51 +44,17 @@ def render_response():
         t1 = r1Sec + (r1Min * 60) + (r1Hr * 3600)
         d2 = 0
         t2 = 0
-        #assigns d2 (desired distance) to equivalent value in meters
-        if(target == "fifteenHundred"):
-            d2 = 1500
-        elif(target == "1Mile"):
-            d2 = 1609
-        elif(target == "threeK"):
-            d2 = 3000
-        elif(target == "3mile"):
-            d2 = 4828
-        elif(target == "fiveK"):
-            d2 = 5000
-        elif(target == "tenK"):
-            d2 = 10000
-        elif(target == "halfMarathon"):
-            d2 = 21097
-        elif(target == "Marathon"):
-            d2 = 42195
-            
-        #checks which race the user selects and proceeds with that distance in mind
-        if(race1 == "fifteenHundred"):
-            t2 = t1 * ((d2 / 1500) * 1.06)
-            prediction = str(datetime.timedelta(seconds=t2))
-        elif(race1 == "1Mile"):
-            t2 = t1 * ((d2 / 1609) * 1.06)
-            prediction = str(datetime.timedelta(seconds=t2))
-        elif(race1 == "threeK"):
-            t2 = t1 * ((d2 / 3000) * 1.06)
-            prediction = str(datetime.timedelta(seconds=t2))
-        elif(race1 == "3mile"):
-            t2 = t1 * ((d2 / 4828) * 1.06)
-            prediction = str(datetime.timedelta(seconds=t2))
-        elif(race1 == "fiveK"):
-            t2 = t1 * ((d2 / 5000) * 1.06)
-            prediction = str(datetime.timedelta(seconds=t2))
-        elif(race1 == "tenK"):
-            t2 = t1 * ((d2 / 10000) * 1.06)
-            prediction = str(datetime.timedelta(seconds=t2))
-        elif(race1 == "halfMarathon"):
-            t2 = t1 * ((d2 / 21097) * 1.06)
-            prediction = str(datetime.timedelta(seconds=t2))
-        elif(race1 == "Marathon"):
-            t2 = t1 * ((d2 / 42195) * 1.06)
-            prediction = str(datetime.timedelta(seconds=t2))
-        else:
-            prediction = "Unable to predict. Try again."
+                
+        for i in races: #Loop through to find target race
+            if target == i: #If the target race matches key \/
+                d2 = races[i] #set the value of d2 as the target race 
+                for j in races: #Then loop through races again to find the matching user entered race
+                    if race1 == j: #If the race is found
+                        t2 = t1 * ((d2 / races[j]) * 1.06) #Calculate using the value of the selected key
+                        prediction = str(datetime.timedelta(seconds=t2)) #Set prediction as the time converted to a string in HH:MM::SS
+            else:
+                pass #Do nothing
+
     #if the user inputs a time for the second data point, calculate including it
     else:
         target = request.args['targetRace']
@@ -77,112 +66,35 @@ def render_response():
         r2Hr = int(request.args['pastRaceHr2'])
         r2Min = int(request.args['pastRaceMin2'])
         r2Sec = int(request.args['pastRaceSec2'])
-        t1 = r1Sec + (r1Min * 60) + (r1Hr * 3600)
-        t2 = r2Sec + (r2Min * 60) + (r2Hr * 3600)
+        t1 = r1Sec + (r1Min * 60) + (r1Hr * 3600) #Convert the entered time to seconds stored as t1
+        t2 = r2Sec + (r2Min * 60) + (r2Hr * 3600) #Convert the entered time for race 2 to seconds stored as t2
         d2 = 0
-        d3 = 0
         t3 = 0
         t4 = 0
-        tAvg = 0
+        tFinal = 0
         
-        #set the desired race as d2
-        if(target == "fifteenHundred"):
-            d2 = 1500
-        elif(target == "1Mile"):
-            d2 = 1609
-        elif(target == "threeK"):
-            d2 = 3000
-        elif(target == "3mile"):
-            d2 = 4828
-        elif(target == "fiveK"):
-            d2 = 5000
-        elif(target == "tenK"):
-            d2 = 10000
-        elif(target == "halfMarathon"):
-            d2 = 21097
-        elif(target == "Marathon"):
-            d2 = 42195
-            
-        #set the second race as d3
-        if(race2 == "fifteenHundred"):
-            d3 = 1500
-        elif(race2 == "1Mile"):
-            d3 = 1609
-        elif(race2 == "threeK"):
-            d3 = 3000
-        elif(race2 == "3mile"):
-            d3 = 4828
-        elif(race2 == "fiveK"):
-            d3 = 5000
-        elif(race2 == "tenK"):
-            d3 = 10000
-        elif(race2 == "halfMarathon"):
-            d3 = 21097
-        elif(race2 == "Marathon"):
-            d3 = 42195
-            
-        #check against the first race given and calculate
-        if(race1 == "fifteenHundred"):
-            t3 = t1 * ((d2 / 1500) * 1.06)
-            t4 = t2 * ((d2 / d3) * 1.06)
-            tAvg = (t3 + t4) / 2
-            prediction = str(datetime.timedelta(seconds=tAvg))
-        elif(race1 == "1Mile"):
-            t3 = t1 * ((d2 / 1609) * 1.06)
-            t4 = t2 * ((d2 / d3) * 1.06)
-            tAvg = (t3 + t4) / 2
-            prediction = str(datetime.timedelta(seconds=tAvg))
-        elif(race1 == "threeK"):
-            t3 = t1 * ((d2 / 3000) * 1.06)
-            t4 = t2 * ((d2 / d3) * 1.06)
-            tAvg = (t3 + t4) / 2
-            prediction = str(datetime.timedelta(seconds=tAvg))
-        elif(race1 == "3mile"):
-            t3 = t1 * ((d2 / 4828) * 1.06)
-            t4 = t2 * ((d2 / d3) * 1.06)
-            tAvg = (t3 + t4) / 2
-            prediction = str(datetime.timedelta(seconds=tAvg))
-        elif(race1 == "fiveK"):
-            t3 = t1 * ((d2 / 5000) * 1.06)
-            t4 = t2 * ((d2 / d3) * 1.06)
-            tAvg = (t3 + t4) / 2
-            prediction = str(datetime.timedelta(seconds=tAvg))
-        elif(race1 == "tenK"):
-            t3 = t1 * ((d2 / 10000) * 1.06)
-            t4 = t2 * ((d2 / d3) * 1.06)
-            tAvg = (t3 + t4) / 2
-            prediction = str(datetime.timedelta(seconds=tAvg))
-        elif(race1 == "halfMarathon"):
-            t3 = t1 * ((d2 / 21097) * 1.06)
-            t4 = t2 * ((d2 / d3) * 1.06)
-            tAvg = (t3 + t4) / 2
-            prediction = str(datetime.timedelta(seconds=tAvg))
-        elif(race1 == "Marathon"):
-            t3 = t1 * ((d2 / 42195) * 1.06)
-            t4 = t2 * ((d2 / d3) * 1.06)
-            tAvg = (t3 + t4) / 2
-            prediction = str(datetime.timedelta(seconds=tAvg))
-        else:
-            prediction = "Unable to predict. Try again."
-            
-    #Convert the value of the target race to a readable equivalent that displays on the response page
-    if(target == "fifteenHundred"):
-        targetRace = "1500M"
-    elif(target == "1Mile"):
-        targetRace = "1 Mile"
-    elif(target == "threeK"):
-        targetRace = "3K"
-    elif(target == "3mile"):
-        targetRace = "3 Mile"
-    elif(target == "fiveK"):
-        targetRace = "5K"
-    elif(target == "tenK"):
-        targetRace = "10K"
-    elif(target == "halfMarathon"):
-        targetRace = "Half Marathon"
-    elif(target == "Marathon"):
-        targetRace = "Marathon"
+        for i in races: #Loop through races to find target race
+            if target == i: #If the target race matches the key \/
+                d2 = races[i] #set d2 equal to the matching value of the key
+                for j in races: #Loop through races again to find race 1
+                    if race1 == j: #If race 1 is found \/
+                        t3 = t1 * ((d2 / races[j]) * 1.06) #Calculate the predicted time based off of race 1 time and distance
+                        for k in races: #Loop through races again to find race 2
+                            if race2 == k: #If race 2 is found \/
+                                t4 = t2 * ((d2 / races[k]) * 1.06) #Calculate the prediction based off of race 2 time and distance
+                                tFinal = (t3 + t4) / 2 #Average the two predictions 
+                                prediction = str(datetime.timedelta(seconds=tFinal)) #Set prediction to the predicted times as a string in HH:MM:SS
+            else:
+                pass #Do nothing
+
     
+    #Convert the name of the race to a readable version which is returned
+    for x in raceResponse:
+        if target == x:
+            targetRace = raceResponse[x]
+        else:
+            pass
+
     return render_template('response.html', response1 = prediction, response2 = targetRace)
     
 if __name__=="__main__":
